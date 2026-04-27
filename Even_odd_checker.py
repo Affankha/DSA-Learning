a28feb0fab78defc0a639149e47a9264d515d887
Affankha <khanaffan34163@gmail.com>
2026-04-27 16:39:14 +0530

    write same code in funtion formate and in bitwise(binory) formate

--- Even_odd_checker.py
+++ Even_odd_checker.py
@@ -1,6 +1,29 @@
-n =37
+# Simple Method
+n =int(input("Enter NO: "))
 
-if(n%2==0):
+if n%2 == 0:
   print(True)
 else:
   print(False)
+
+#  Using Funtion
+def isEven(n):
+    if n % 2 == 0:
+       return True
+       # print(True)
+    else:
+        return False
+      # print(False)
+      
+print(isEven(12))
+
+
+#Through Bitwise operator
+# x ko binory me connvert karenga or 1 ke sath some karenga, Even ke last me 0 aata binory me odd ke last me 1 ata 
+x=91
+if (x & 1) ==0:
+  print("True")
+else: 
+  print("False")
+
+
