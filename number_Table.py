09b7a6cbeb923f7212e68e8c3962432b7858ffb7
Affankha <khanaffan34163@gmail.com>
2026-04-29 08:42:34 +0530

    print the table a number

---
+++ number_Table.py
@@ -0,0 +1,26 @@
+# print the table a input number
+
+n= int(input("enter any number: "))
+
+table =[]
+
+i=1
+while i<=10:
+  table.append(i*n)
+  i+=1
+
+for i in range(len(table)):
+    print(table[i])
+
+
+
+
+
+
+# 2nd method
+print("2nd method")
+
+i=1
+while i<=10:
+  print(i*n)
+  i=i+1
