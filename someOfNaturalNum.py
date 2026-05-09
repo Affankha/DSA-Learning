dea2de9b20ada504824e03c894a0982c9f8568d1
Affankha <khanaffan34163@gmail.com>
2026-05-10 00:04:24 +0530

    solve problem of some of natural number and its squre

---
+++ someOfNaturalNum.py
@@ -0,0 +1,9 @@
+# # find some of n natural numbers
+
+n =int(input("enter any Natural Number: "))
+
+some = int(n*(n+1))/2
+
+print(int(some))
+
+
---
+++ sumofSquaresOfNNatualNUm.py
@@ -0,0 +1,10 @@
+# calculate some of squares of na natural nomber
+n =int(input("enter any natural no:"))
+
+
+some =0
+for i in range(n):
+    print(i)
+    i =i*i
+    some = some+i
+print(some)
